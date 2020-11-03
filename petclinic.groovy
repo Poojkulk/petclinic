914e97920fe5dd6d2de512189570075e58831134
Poojkulk <50866928+Poojkulk@users.noreply.github.com>
2020-11-03 17:05:07 +0530

    Update petclinic.groovy

--- petclinic.groovy
+++ petclinic.groovy
@@ -1,11 +1,20 @@
-job('SPGOLU') {
-    scm {
-        git('https://github.com/Poojkulk/Bamboo-spec.git')
-    }
-    triggers {
+pipeline {
+    agent any
+
+    stages {
+        stage('cloning repo ... ') {
+            steps {
+                git 'https://github.com/Poojkulk/Bamboo-spec.git'
+            }
+        }
+         triggers {
         scm('H/5 * * * *')
     }
     steps {
         maven('-e clean test')
     }
+    }
+      
 }
+
+
